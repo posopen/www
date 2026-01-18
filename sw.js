2d44c3179b08de77df6f3574addb3ea619d5d59b
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-01-19 00:00:00 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'posopen-shell-v1';
+const SHELL_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL_ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      cached => cached || fetch(event.request)
+    )
+  );
+});
